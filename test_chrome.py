46ab7984955930c599a1410b68447560a4489a30
Alexander L <echtopy@gmail.com>
2023-04-27 14:42:06 +0300

    conftest.py added. test_chrome.py added. test_firefox.py added.

---
+++ test_chrome.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+base_url = 'https://homepro.herokuapp.com/index.php'
+expected_url = 'https://homepro.herokuapp.com/order.php'
+expected_success_url = "https://homepro.herokuapp.com/orderconfirm.php"
+
+
+@pytest.mark.parametrize(("name", "lastname", "phone", "email"),
+                         [
+                            ("Vladimir", "LastName2", "+7-800-555-35-35", "echtopy@gmail.com"),
+                            ("Anton", "LastName2", "+7-800-555-35-35", "echtopy@gmail.com"),
+                            ("Mia", "LastName2", "+7-800-555-35-35", "echtopy@gmail.com"),
+                            ("Crabs", "LastName2", "+7-800-555-35-35", "echtopy@gmail.com"),
+                            ("Alex", "LastName2", "+7-800-555-35-35", "echtopy@gmail.com")
+                        ])
+@pytest.mark.regressiontest
+def test_schedule_success(browser_chrome, name, lastname, phone, email):
+    browser_chrome.get(base_url)
+    browser_chrome.find_element(By.LINK_TEXT, "Schedule an Appointment").click()
+    assert expected_url == browser_chrome.current_url
+    jobtype_dropdown = Select(browser_chrome.find_element(By.NAME, "job_type"))
+    jobtype_dropdown.select_by_visible_text("Decorating")
+    browser_chrome.find_element(By.NAME, "first_name").send_keys(name)
+    browser_chrome.find_element(By.NAME, "last_name").send_keys(lastname)
+    browser_chrome.find_element(By.NAME, "phone").send_keys(phone)
+    browser_chrome.find_element(By.NAME, "email").send_keys(email)
+    browser_chrome.find_element(By.XPATH, "//input[@value = 'Schedule My consultation']").click()
+    assert expected_success_url == browser_chrome.current_url
